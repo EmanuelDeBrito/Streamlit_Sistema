91ec9198a56b97c77dde384c8342e1ea13c35881
Emanuel <108434968+EmanuelDeBrito@users.noreply.github.com>
2023-02-22 21:34:24 -0300

    Update banana.py

--- banana.py
+++ banana.py
@@ -1,13 +1,11 @@
-# Importações
-import streamlit as st # Importando o streamlit com o apelido de st
-from streamlit_extras.let_it_rain import rain # Do streamlit extras importando um componente
-from PIL import Image # Importando Image da biblioteca de imagem PILLOW
-import requests # Importando a biblioteca requests
-import streamlit_lottie # Importando o lottie
-from streamlit_lottie import st_lottie # do lottie importa o st_lottie
+import streamlit as st 
+from streamlit_extras.let_it_rain import rain 
+from PIL import Image 
+import requests 
+import streamlit_lottie
+from streamlit_lottie import st_lottie 
 
 
-# Função para transformar os links json do lottie em animações
 def lottieurl(url: str):
     r = requests.get(url)
     if r.status_code != 200:
@@ -15,7 +13,6 @@ def lottieurl(url: str):
     return r.json()
 
 
-# Links dos json
 banana = "https://assets6.lottiefiles.com/private_files/lf30_vuxs5lpt.json"
 rodolfo = "https://assets1.lottiefiles.com/packages/lf20_7aq7s6rs.json"
 oswaldo = "https://assets10.lottiefiles.com/private_files/lf30_WVVTq8.json"
@@ -24,7 +21,6 @@ jonas = "https://assets8.lottiefiles.com/packages/lf20_2nbubmkc.json"
 jussimar = "https://assets2.lottiefiles.com/packages/lf20_rgrylcip.json"
 wellington = "https://assets5.lottiefiles.com/packages/lf20_E0GQM0.json"
 
-# Transformando os links json em animações
 banana_json = lottieurl(banana)
 rodolfo_json = lottieurl(rodolfo)
 oswaldo_json = lottieurl(oswaldo)
@@ -33,10 +29,8 @@ jonas_json = lottieurl(jonas)
 jussimar_json = lottieurl(jussimar)
 wellington_json = lottieurl(wellington)
 
-# Um titulo do BananaWeb
 st.title('BananaWeb')
 
-# Fazendo um rain na página
 rain(
     emoji="🍌",
     font_size=34,
@@ -44,7 +38,6 @@ rain(
     animation_length="infinite",
     )
 
-# colocando um texto em formato de linguagem
 codigo = ''' def bananinha():
     print("Informações Pessoais")
     informacoes = [{'Nome': 'Augusto Fabiano Abranches'}
@@ -54,47 +47,31 @@ codigo = ''' def bananinha():
     {'Linguagem': 'Mestre do Banco de Dados e Arduíno'}]
 '''
 
-# Transforamando esse código de cima em um st.code para ficar bonito na página
 st.code(codigo, language="python")
 
-# Atribuindo a uma variavel o caminho da imagem
 programacao = Image.open(r"C:\Users\danil\Downloads\dev.jpg")
 
-# Colocando a imagem na tela
 st.image(programacao)
 
-# Criando colunas
 col1, col2, col3 = st.columns(3)
 
-# Na coluna 1
 with col1:
-    # Um header
     st.header('BananaWeb')
-    # Um texto
     st.write('Como foi pedido para fazer um sisteminha pensei em fazer um simples Website sobre o banana')
 
-# Na coluna 2
 with col2:
-    # Um header
     st.header('Aulas')
-    # Um texto
     st.write('As aulas ajudam muito professor apesar de serem um pouco loucas elas ajudam bastante, pega leve no TCC =)')
 
-# Na coluna 3
 with col3:
-    # Um header
     st.header('Agradecimentos')
-    # Um texto
     st.write('Agradeço a todos professores pelas máterias passadas que foram muito importantes para o crescimento')
 
-# Colocando mais um header
 st.header('Professores')
 
-# Fazendo tabs
 tab1, tab2, tab3, tab4, tab5, tab6, tab7 = st.tabs(["Bananinha", "Jonas", "Jussimar",
                                                     "Oswaldo", "Paulo", "Rodolfo", "Wellington"]) # Quebrei a linha
 
-# Agora vou fazer para cada tab uma descrição
 with tab1:
     st.header('Augusto')
     st.write('Apesar da aula ser um pouco maluca as máterias são sempre dadas com maestria pelo professor sempre sendo muito engraçadas e utéis')
